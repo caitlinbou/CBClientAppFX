cf2fe57809c8f121cf7a285dfba29ede18a6b319
caitlinbou <caitlin.bouroncle@gmail.com>
2024-02-04 11:33:03 -0700

    CleanUp

--- src/Controller/CountriesController.java
+++ src/Controller/CountriesController.java
@@ -6,17 +6,11 @@ import Model.Countries;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CountriesController implements Initializable {
-    public TableColumn idCol;
-    public TableColumn nameCol;
-    public TableView dataTable;
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
